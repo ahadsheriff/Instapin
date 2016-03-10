5bd0860b12961729316ddbfb52cfe9e806b1a92b
ahadsheriff <ahadsheriff@gmail.com>
2016-03-10 17:35:57 -0500

    Meteor server configuration

---
+++ server/fixtures.js
@@ -0,0 +1,57 @@
+if (Posts.find().count() === 0) {
+
+  Posts.insert({
+    title: 'Developing in Meteor',
+    description: 'Meteor is an awesome development framework',
+    media_url: 'https://d14xs1qewsqjcd.cloudfront.net/assets/i/home-animation/laptop.png',
+    url: 'http://meteor.com',
+  });
+
+  Posts.insert({
+    title: 'Meteor Packages',
+    description: 'Meteor packages in the Atmosphere',
+    media_url: 'https://d14xs1qewsqjcd.cloudfront.net/assets/i/features/guide-laptop.png',
+    url: 'http://www.atmosphere.com'
+  });
+
+  Posts.insert({
+    title: 'Developing in Meteor',
+    description: 'Meteor is an awesome development framework',
+    media_url: 'https://d14xs1qewsqjcd.cloudfront.net/assets/i/features/guide-laptop.png',
+    url: 'http://meteor.com',
+  });
+
+  Posts.insert({
+    title: 'Meteor Packages',
+    description: 'Meteor packages in the Atmosphere',
+    media_url: 'https://d14xs1qewsqjcd.cloudfront.net/assets/i/home-animation/laptop.png',
+    url: 'http://www.atmosphere.com'
+  });
+  Posts.insert({
+    title: 'Developing in Meteor',
+    description: 'Meteor is an awesome development framework',
+    media_url: 'https://d14xs1qewsqjcd.cloudfront.net/assets/i/features/guide-laptop.png',
+    url: 'http://meteor.com',
+  });
+
+  Posts.insert({
+    title: 'Meteor Packages',
+    description: 'Meteor packages in the Atmosphere',
+    media_url: 'https://d14xs1qewsqjcd.cloudfront.net/assets/i/home-animation/laptop.png',
+    url: 'http://www.atmosphere.com'
+  });
+
+  Posts.insert({
+    title: 'Developing in Meteor',
+    description: 'Meteor is an awesome development framework',
+    media_url: 'https://d14xs1qewsqjcd.cloudfront.net/assets/i/home-animation/laptop.png',
+    url: 'http://meteor.com',
+  });
+
+  Posts.insert({
+    title: 'Meteor Packages',
+    description: 'Meteor packages in the Atmosphere',
+    media_url: 'https://d14xs1qewsqjcd.cloudfront.net/assets/i/features/guide-laptop.png',
+    url: 'http://www.atmosphere.com'
+  });
+}
